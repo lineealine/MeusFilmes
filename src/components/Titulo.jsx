e49fc4d7cf661f98742d3897170a9f63d201639b
lineealine <lineealine156@gmail.com>
2025-09-25 10:17:10 -0300

    começo Co-authored-by: otaviofck <otaviofck@users.noreply.github.com>

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+import {Text} from 'react-native'
+
+export default function Titulo(props) {
+
+    return (
+        <Text style={{fontSize: 30, textAlign: 'center', fontWeight: 'bold', justifyContent: 'flex-start '}}>
+            {props.texto}
+        </Text>
+    )
+
+}
---
+++ src/pages/SemanaInclusao.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, View, Image } from 'react-native';
+import Titulo from '../components/Titulo';
+
+export default function SemanaInclusao() {
+  return (
+    <View style={styles.container}>
+      <Titulo texto={'Semana da Inclusão de Pessoa com Deficiência'} />
+      <Image
+        width={100}
+        height={100}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
